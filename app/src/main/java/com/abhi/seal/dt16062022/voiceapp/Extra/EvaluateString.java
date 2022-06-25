63d08f8db135b41abdb41c9d5c03da149ef80711
Abhisek <abhiseal45@gmail.com>
2022-06-25 17:15:03 +0530

    25062022

--- app/src/main/java/com/abhi/seal/dt16062022/voiceapp/Extra/EvaluateString.java
+++ app/src/main/java/com/abhi/seal/dt16062022/voiceapp/Extra/EvaluateString.java
@@ -10,28 +10,22 @@ public class EvaluateString
         char[] tokens = expression.toCharArray();
 
         // Stack for numbers: 'values'
-        Stack<Integer> values = new
-                Stack<Integer>();
+        Stack<Integer> values = new Stack<Integer>();
 
         // Stack for Operators: 'ops'
-        Stack<Character> ops = new
-                Stack<Character>();
+        Stack<Character> ops = new Stack<Character>();
 
         for (int i = 0; i < tokens.length; i++)
         {
 
-            // Current token is a
-            // whitespace, skip it
+
             if (tokens[i] == ' ')
                 continue;
 
-            // Current token is a number,
-            // push it to stack for numbers
             if (tokens[i] >= '0' &&
                     tokens[i] <= '9')
             {
-                StringBuffer sbuf = new
-                        StringBuffer();
+                StringBuffer sbuf = new StringBuffer();
 
                 // There may be more than one
                 // digits in number
@@ -42,23 +36,12 @@ public class EvaluateString
                 values.push(Integer.parseInt(sbuf.
                         toString()));
 
-                // right now the i points to
-                // the character next to the digit,
-                // since the for loop also increases
-                // the i, we would skip one
-                //  token position; we need to
-                // decrease the value of i by 1 to
-                // correct the offset.
                 i--;
             }
 
-            // Current token is an opening brace,
-            // push it to 'ops'
             else if (tokens[i] == '(')
                 ops.push(tokens[i]);
 
-                // Closing brace encountered,
-                // solve entire brace
             else if (tokens[i] == ')')
             {
                 while (ops.peek() != '(')
@@ -74,11 +57,7 @@ public class EvaluateString
                     tokens[i] == '*' ||
                     tokens[i] == '/')
             {
-                // While top of 'ops' has same
-                // or greater precedence to current
-                // token, which is an operator.
-                // Apply operator on top of 'ops'
-                // to top two elements in values stack
+
                 while (!ops.empty() &&
                         hasPrecedence(tokens[i],
                                 ops.peek()))
@@ -91,22 +70,16 @@ public class EvaluateString
             }
         }
 
-        // Entire expression has been
-        // parsed at this point, apply remaining
-        // ops to remaining values
         while (!ops.empty())
             values.push(applyOp(ops.pop(),
                     values.pop(),
                     values.pop()));
 
-        // Top of 'values' contains
-        // result, return it
+
         return values.pop();
     }
 
-    // Returns true if 'op2' has higher
-    // or same precedence as 'op1',
-    // otherwise returns false.
+
     public static boolean hasPrecedence(
             char op1, char op2)
     {
@@ -119,9 +92,7 @@ public class EvaluateString
             return true;
     }
 
-    // A utility method to apply an
-    // operator 'op' on operands 'a'
-    // and 'b'. Return the result.
+
     public static int applyOp(char op,
                               int b, int a)
     {
